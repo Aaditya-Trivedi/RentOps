76452233092f19458aa26f95e2a275ea64c3fbe7
Aaditya Trivedi <adityatrivedi972@gmail.com>
2026-01-13 09:37:53 +0530

    ragister all apps into settings.py

--- Rent/Rent/settings.py
+++ Rent/Rent/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'inventory',
+    'notifications',
+    'rentals',
+    'shops',
+    'subscriptions',
 
 ]
 
