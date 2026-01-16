5272ba0755d5ce3e8a77a0bdcd23e51d25df6c01
Aaditya Trivedi <adityatrivedi972@gmail.com>
2026-01-16 10:00:20 +0530

    change in Plan and db

--- Rent/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import shop_login, shop_logout
-
-urlpatterns = [
-    path('shop/login/', shop_login, name='shop_login'),
-    path('shop/logout/', shop_logout, name='shop_logout'),
-]
--- Rent/shops/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import create_shop
-
-urlpatterns = [
-    path('create/', create_shop, name='create_shop'),
-]
